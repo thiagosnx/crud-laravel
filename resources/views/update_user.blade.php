f11674edf4111a59ee9bfc0e882adfe6c8e1ef7f
thiagosnx <thiagosantanacq@gmail.com>
2023-10-26 16:15:06 -0300

    fix: CRUD

---
+++ resources/views/update_user.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    <form action="/updateuser/{{ $usuario->id }}" method="POST"> <!--passamos a route na action-->
+        <!--recebendo o id do msm jeito do name e email la embaixo-->
+        @csrf <!-- token de segurança exigido para post do laravel (so funciona com blade) -->
+        @method("PUT") <!--method put do laravel-->
+        <label for="">Nome:</label>
+        <input type="text" placeholder="nome" name="name" value="{{ $usuario->name }}"> <!--input ja recebendo os valores do usuario-->
+        <br>
+        <br>
+        <label for="">Email:</label>
+        <input type="text" placeholder="email" name="email" value="{{ $usuario->email }}"><!--input ja recebendo os valores do usuario-->
+        <br>
+        <button>Atualizar</button>        
+</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Http\Request;
+use App\Models\Uzer;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //retorna o arquivo view welcome.blade.php
+    //@requestmapping?
 });
+
+
+Route::post('/register', function (Request $data) { //criando o mapping com o metodo request do http
+    //a rota /register é usada so para o funcionamento do metodo, não é possivel acessar pela url
+    Uzer::create([ //criando um usuario a partir do meu model Uzer
+        'name'=>$data->name,
+        'email'=>$data->email
+    ]);
+    echo "Usuário cadastrado!";
+    header('Location: /users');
+
+});
+
+Route::get('/users', function(){ //getallusers
+    $allusers = Uzer::all();
+    echo $allusers;
+});
+
+Route::get('/users/{id}', function ($id){ //getusersbyid
+    $usuario = Uzer::findOrFail($id);
+    echo $usuario->name;
+    echo "<br>";
+    echo $usuario->email;
+
+});
+
+Route::get('/update/{id}', function ($id){//rota de get so para retornar um html pra editar o usuario
+    $usuario = Uzer::findOrFail($id);
+    return view('update_user', ['usuario' => $usuario]);
+});
+
+Route::put('/updateuser/{id}' , function (Request $data, $id){//recebendo dados do request e o id do usuario
+    $usuario = Uzer::findOrFail($id);
+    $usuario->name = $data->name;
+    $usuario->email = $data->email;
+    $usuario->save();
+    echo "Chama!";
+}); 
+
+Route::get('/delete{id}', function ($id){ //getuserbyid e ja deleta auto qnd coloca a url
+    $usuario = Uzer::findOrFail($id);
+    $usuario->delete();
+    echo "Excluido!";
+    //fazer metodo para delete com click
+});
+
+
+
