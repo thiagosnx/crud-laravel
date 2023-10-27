0960e26ec1a9f424c3953e61c863a156fe5b733f
thiagosnx <thiagosantanacq@gmail.com>
2023-10-27 14:00:54 -0300

    refactor: routes/controller

--- resources/views/update_user.blade.php
+++ resources/views/update_user.blade.php
@@ -6,16 +6,16 @@
     <title>CRUD</title>
 </head>
 <body>
-    <form action="/updateuser/{{ $usuario->id }}" method="POST"> <!--passamos a route na action-->
+    <form action="/updateuser/{{ $users->id }}" method="POST"> <!--passamos a route na action-->
         <!--recebendo o id do msm jeito do name e email la embaixo-->
         @csrf <!-- token de segurança exigido para post do laravel (so funciona com blade) -->
         @method("PUT") <!--method put do laravel-->
         <label for="">Nome:</label>
-        <input type="text" placeholder="nome" name="name" value="{{ $usuario->name }}"> <!--input ja recebendo os valores do usuario-->
+        <input type="text" placeholder="nome" name="name" value="{{ $users->name }}"> <!--input ja recebendo os valores do usuario-->
         <br>
         <br>
         <label for="">Email:</label>
-        <input type="text" placeholder="email" name="email" value="{{ $usuario->email }}"><!--input ja recebendo os valores do usuario-->
+        <input type="text" placeholder="email" name="email" value="{{ $users->email }}"><!--input ja recebendo os valores do usuario-->
         <br>
         <button>Atualizar</button>        
 </form>
--- routes/web.php
+++ routes/web.php
@@ -3,56 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Models\Uzer;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome'); //retorna o arquivo view welcome.blade.php
-    //@requestmapping?
-});
+Route::get('/', [UserController::class, 'index'] );
 
+Route::post('/register', [UserController::class, 'createUser']);  
 
-Route::post('/register', function (Request $data) { //criando o mapping com o metodo request do http
-    //a rota /register é usada so para o funcionamento do metodo, não é possivel acessar pela url
-    Uzer::create([ //criando um usuario a partir do meu model Uzer
-        'name'=>$data->name,
-        'email'=>$data->email
-    ]);
-    echo "Usuário cadastrado!";
-    header('Location: /users');
+Route::get('/users', [UserController::class, 'allUsers'] ); 
 
-});
+Route::get('/users/{id}', [UserController::class, 'getUserById'] );
 
-Route::get('/users', function(){ //getallusers
-    $allusers = Uzer::all();
-    echo $allusers;
-});
+Route::get('/users/update/{id}', [UserController::class, 'updateUsers']);
 
-Route::get('/users/{id}', function ($id){ //getusersbyid
-    $usuario = Uzer::findOrFail($id);
-    echo $usuario->name;
-    echo "<br>";
-    echo $usuario->email;
+Route::put('/updateuser/{id}' , [UserController::class, 'updateUsersMethod']);
 
-});
-
-Route::get('/update/{id}', function ($id){//rota de get so para retornar um html pra editar o usuario
-    $usuario = Uzer::findOrFail($id);
-    return view('update_user', ['usuario' => $usuario]);
-});
-
-Route::put('/updateuser/{id}' , function (Request $data, $id){//recebendo dados do request e o id do usuario
-    $usuario = Uzer::findOrFail($id);
-    $usuario->name = $data->name;
-    $usuario->email = $data->email;
-    $usuario->save();
-    echo "Chama!";
-}); 
-
-Route::get('/delete{id}', function ($id){ //getuserbyid e ja deleta auto qnd coloca a url
-    $usuario = Uzer::findOrFail($id);
-    $usuario->delete();
-    echo "Excluido!";
-    //fazer metodo para delete com click
-});
+Route::get('/users/delete/{id}', [UserController::class, 'deleteUser']);
 
 
 
